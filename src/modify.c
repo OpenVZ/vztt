e043d2e5810f116dcfb31d33d885e6d9bf4be553
Denis Silakov <dsilakov@virtuozzo.com>
2020-04-17 16:22:54 +0300

    Revert "Revert "Set ploop trusted flag on cache creation and modify procedures, see #PSBM-98307""
    This reverts commit 0b5083c2fe7eabf9b3358ae205d82eee9a26f3e0.

--- src/modify.c
+++ src/modify.c
@@ -174,6 +174,9 @@ static int cmd_modify(
 	if ((rc = tmpl_lock(&gc, tmpl->base, 
 			LOCK_READ, opts_vztt->flags, &lockdata)))
 		goto cleanup_3;
+
+	set_trusted(ctid, "1");
+
 	switch(cmd) {
 		case VZPKG_INSTALL:
 			/* Install packages into VE */
@@ -237,6 +240,7 @@ static int cmd_modify(
 				goto cleanup_4;
 			break;
 	}
+	set_trusted(ctid, "0");
 	tmpl_unlock(lockdata, opts_vztt->flags);
 
 	/* fill output packages arrays */
@@ -271,6 +275,7 @@ static int cmd_modify(
 
 	goto cleanup_3;
 cleanup_4:
+	set_trusted(ctid, "0");
 	tmpl_unlock(lockdata, opts_vztt->flags);
 cleanup_3:
 	sigaction(SIGINT, &act_int, NULL);
